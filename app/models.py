6e7ef02c44bcae5d15e2815fca5028b9ca4bceb0
maureenndiema <ndiemam@gmail.com>
2022-05-09 01:32:07 +0300

    import db, password generator and user class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
-from . import db
+from . import db, login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin,current_user
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-
-    def __repr__(self):
-        return f'User {self.username}'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    secure_password = db.Column(db.String(255), nullable=False)
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote', backref='user', lazy='dynamic')
+    downvote = db.relationship('Downvote', backref='user', lazy='dynamic')
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String) 
