08090cd65821b59833d43d485c419aa4117dc69f
maureenndiema <ndiemam@gmail.com>
2022-05-08 22:31:22 +0300

    create login function

--- app/main/views.py
+++ app/main/views.py
@@ -17,4 +17,21 @@ def profile(uname):
     if user is None:
         abort(404)
 
-    return render_template('profile/profile.html',user = user,pitches=pitches)  
+    return render_template('profile/profile.html',user = user,pitches=pitches) 
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = updateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+           
+        return redirect(url_for('.profile',uname=user.username)) 
