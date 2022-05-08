9d6bee8973f2e4c5511549459080b86d2d421e6d
maureenndiema <ndiemam@gmail.com>
2022-05-09 01:48:09 +0300

    remove entries from database

--- app/models.py
+++ app/models.py
@@ -37,3 +37,96 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'Pitch hub'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255), nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    category = db.Column(db.String(255), index = True,nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    upvote = db.relationship('Upvote', backref='pitch', lazy='dynamic')
+    downvote = db.relationship('Downvote', backref='pitch', lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"Pitch {self.title}"
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    comment = db.Column(db.Text(),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+        return comments
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'comment: {self.comment}'
+
+
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+        
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
