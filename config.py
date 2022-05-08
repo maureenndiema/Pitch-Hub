c1926e904f7b3d50588c49baeb1134b4e9231042
maureenndiema <ndiemam@gmail.com>
2022-05-08 15:07:17 +0300

    add child class to config.py

--- config.py
+++ config.py
@@ -1,3 +1,48 @@
 import os
-class config(object):
-    SQALCHEMY_DATABASE_URL= os.get('SECRET_KEY')
+class config:
+    debug = True
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URL= 'postgresql+psycopg2::1234@localhost'
+
+     #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://:1234@localhost/'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://:@localhost/'
+    
+    
+    DEBUG = True
+    ENV = 'development'
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
