043f16c24bfc289c35cd4b2ebf4d5400905d15c4
maureenndiema <ndiemam@gmail.com>
2022-05-09 00:44:14 +0300

    import db instance from _init_.py

--- manage.py
+++ manage.py
@@ -1,4 +1,14 @@
 from app import create_app
 from flask_script import Manager,Server
+from app import create_app,db
+# .....
+from app.models import User
+# ...
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
 
 
