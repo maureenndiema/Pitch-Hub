27792eae78c67764d1d19f941b763b20d3019aed
maureenndiema <ndiemam@gmail.com>
2022-05-09 01:35:39 +0300

    create save password functions

--- app/models.py
+++ app/models.py
@@ -15,3 +15,25 @@ class User(UserMixin, db.Model):
     downvote = db.relationship('Downvote', backref='user', lazy='dynamic')
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String) 
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
