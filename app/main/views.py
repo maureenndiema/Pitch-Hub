e46a6b39a580fafd0870fb60f25f63877001dd33
maureenndiema <ndiemam@gmail.com>
2022-05-08 22:29:27 +0300

    create route function for username&id

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,17 @@ from flask_login import login_required, current_user
 from . import main
 from .forms import PostForm, CommentForm, UpdateProfile
 from ..models import Post, Comment, User, Upvote, Downvote
+
+@main.route('/')
+def index():
+
+    return render_template('index.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    pitches = Pitch.query.filter_by(user_id = user.id).all()
+    if user is None:
+        abort(404)
+
+    return render_template('profile/profile.html',user = user,pitches=pitches)  
