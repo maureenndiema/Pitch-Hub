d5ebf92f8a5c93cfd2bd19f9f89464d1cab34caf
maureenndiema <ndiemam@gmail.com>
2022-05-09 00:26:05 +0300

    create user class for new users in models.py

--- app/main/views.py
+++ app/main/views.py
@@ -19,19 +19,3 @@ def profile(uname):
 
     return render_template('profile/profile.html',user = user,pitches=pitches) 
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
-@login_required
-def update_profile(uname):
-    user = User.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
-
-    form = updateProfile()
-
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        db.session.add(user)
-        db.session.commit()
-           
-        return redirect(url_for('.profile',uname=user.username)) 
